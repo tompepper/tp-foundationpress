4d3aa073d66928e3e9a5f8c81ed6611eb0b6a64d
Tom Pepper <pepper.tom@gmail.com>
2017-09-13 17:11:24 -0400

    - Added Open Sans font - Added new scss folders for buttons, footer, header, typography and plugin-overrides/contact - Removed page-full template - Modified atoms scss

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 <!doctype html>
 <html class="no-js" <?php language_attributes(); ?> >
 	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<?php wp_head(); ?>
@@ -49,7 +50,7 @@
 				<?php endif; ?>
 			</div>
 		</nav>
-		
+
 	</header>
 
 	<section class="container">
